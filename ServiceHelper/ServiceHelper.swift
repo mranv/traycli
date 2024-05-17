0eff94442062160150d25efe0e2b7072f1f15e96
mranv <iamanubhavgain@gmail.com>
2024-05-17 14:27:01 +0530

    Added: cli based application for macos to fetch latest status of gatekeeper | wazuh agent | osquery using xpc.

---
+++ ServiceHelper/ServiceHelper.swift
@@ -0,0 +1,18 @@
+//
+//  ServiceHelper.swift
+//  ServiceHelper
+//
+//  Created by Anubhav Gain on 17/05/24.
+//
+
+import Foundation
+
+/// This object implements the protocol which we have defined. It provides the actual behavior for the service. It is 'exported' by the service to make it available to the process hosting the service over an NSXPCConnection.
+class ServiceHelper: NSObject, ServiceHelperProtocol {
+    
+    /// This implements the example protocol. Replace the body of this class with the implementation of this service's protocol.
+    @objc func performCalculation(firstNumber: Int, secondNumber: Int, with reply: @escaping (Int) -> Void) {
+        let response = firstNumber + secondNumber
+        reply(response)
+    }
+}
---
+++ ServiceHelper/ServiceManager.swift
@@ -0,0 +1,8 @@
+//
+//  ServiceManager.swift
+//  traycli
+//
+//  Created by Anubhav Gain on 17/05/24.
+//
+
+import Foundation
